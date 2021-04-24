a5e339540320a222e06d7e0be0f49e916167ceea
Manuel Aguilera Magaña <manuel_aguilerita@hotmail.com>
2021-04-24 02:10:19 -0500

    masss

--- src/controllers/temporaryuser.controller.js
+++ src/controllers/temporaryuser.controller.js
@@ -74,6 +74,7 @@ export const createUser = async (req, res) => {
                     usuario: req.body.usuario, 
                     emailPersonal: req.body.emailPersonal,
                     tokenN: req.body.tokenN,
+                    estadoInsc: "En revisión",
                 });
                 const usersave = await newUser.save();
                 const newDocumento = new Documento({
